25440f747526f0ed31a999136ec412cb0a2427b6
Bloo00 <kabin.yangg@gmail.com>
2021-11-14 01:08:02 -0800

    made collision i think

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ ctx.lineWidth           = 5;
 
 game.setAttribute("height", getComputedStyle(game)["height"]);
 game.setAttribute("width", getComputedStyle(game)["width"]);
-
+console.log(game.width,game.height);
 // ==== Variables ====
 
 let coins               = 0;
@@ -31,10 +31,18 @@ let pinkCat     = new Image();
 let purpleCat   = new Image();
 let orangeCat   = new Image();
 
-// ==== Entities ====
+blackCat.src    = "Black.png";
+greenCat.src    = "Green.png";
+tealCat.src     = "Teal.png";
+pinkCat.src     = "Pink.png";
+purpleCat.src   = "Purple.png";
+orangeCat.src   = "Orange.png";
 
+
+// ==== Entities ====
+console.log(game.height);
 class Prize {
-    constructor(x, y, z, width, height, color) {
+    constructor(x, y, z, width, height, color, cat) {
         this.x          = x;
         this.y          = y;
         this.z          = z;
@@ -43,11 +51,8 @@ class Prize {
         this.caught     = false;                        
         this.color      = color;
 
-        clawImg.src     = 'Crane_1.png';
-        
-
-        function aveWidth(x, width) {
-            return (x+width)/2;
+        function hitbox(x,y) {
+            return ;
         }
 
         this.render = function () {
@@ -66,7 +71,7 @@ class Claw {
         this.width      = width;                        // i want the average of this or the base of the claw to
         this.caught     = false;
         let clawImg     = new Image();
-        clawImg.src     = 'Crane_1.png';
+        clawImg.src     = 'Crane_4.png';
 
         this.render     = function () {
             //ctx.fillStyle = "#bada55"; // 
@@ -74,8 +79,8 @@ class Claw {
             ctx.drawImage(clawImg, this.x, this.y, this.width, this.height);
         }
 
-        function aveWidth(x, width) {
-            return (x+width)/2;
+        function aveWidth(x) {
+            return (x+450)/2;
         }
     }
 }
@@ -96,46 +101,40 @@ class Machine {
 // ==== Event listener ====
 
 window.addEventListener("load", function(e) {       // should check to see if the dom loaded
-    claw = new Claw(20, 50, 0, 50, 50);                  // makes a new claw with the size of that
-                                                                // the 0 is the z axis and should go to 5
+    claw = new Claw(20, -300, 0, 450, 700);         // makes a new claw with the size of that
+                                                    // the 0 is the z axis and should go to 5
     prizeMaker();
-    const runGame = setInterval(gameLoop, 1);               // game loop set at 1 ms
-    
-    //console.log(claw);
+    const runGame = setInterval(gameLoop, 1);       // game loop set at 1 ms
 })
 
 // ==== Game Processes ====
 
 function gameLoop() {
     ctx.clearRect(0,0,game.width,game.height);  // clears canvas
-    claw.render();           // makes a new claw
+    claw.render();                              // makes a new claw
 }
 
 // ==== Movement of the Claw ====
 
 document.addEventListener("keydown", movementHandler);
+// document.addEventListener("keyup", movementHandler);
 
-function movementHandler (e) {
+function movementHandler (e) { 
     //console.log("movement", e.key);
     //console.log(e);
 
     switch(e.key){            
-        case "w":               // wanna mmake it move to the "z" direction  get further 
-            console.log(claw.z, claw.height, claw.width)
+        case "w":                               // wanna mmake it move to the "z" direction  get further 
             if (claw.z === 5){  
-                claw.z          = 5;
+                claw.z              = 5;
             }else{
                 if (claw.z < 5){
                     claw.z          += 1;
-                    claw.height     -= 4;
-                    claw.width      -= 4;
-                    claw.x          += 2;
+                    claw.height     -= 30;
+                    claw.width      -= 30;
+                    claw.x          += 15;
                 }
-                    
-            }
-            
-            
-            //claw.y - 10 >= 0 ? (claw.y -= 10) : null;
+            }//claw.y - 10 >= 0 ? (claw.y -= 10) : null;
             break;
 
         case "a" :              // wanna mmake it move to the "z" direction get futher 
@@ -148,30 +147,33 @@ function movementHandler (e) {
         }else{
             if (claw.z > 0){
                 claw.z          -= 1;
-                claw.height     += 4;
-                claw.width      += 4;
-                claw.x          -= 2;
+                claw.height     += 30;
+                claw.width      += 30;
+                claw.x          -= 15;
             }
-                
         }
-        
             break;
-
+            
         case "d":               // wanna mmake it move to the "z" direction get closer
             claw.x + 10 <= game.width ? (claw.x += 10) : null;
             break;
 
         case " ":               // should make the claw take away a try drop the claw and drop it to the box
-            claw.y - 10 >= 0 ? (claw.y -= 10) : null;
+        console.log("something");
+        console.log(game.height);
+        console.log(claw.y);
+        
+            claw.y + 10 >= game.height ? (claw.y += 10) : null;
             if (claw.y - 10 >= 0 ? (claw.y -= 10) : null) {     // whille its moving 
-                for (let i =0; i < prizes.length; i++){ // make the claw check for colisions
+                for (let i =0; i < prizes.length; i++){         // make the claw check for colisions
                     hitDetection(claw, prizes[i]);
                 }
+                if (claw.y === game.height || claw.y < game.height) {
+                    claw.y === game.height;
+                }                               // returneto moansodfabyuand;basdfgoinoin';pasfdg
             }
-
             break;
     }
-    
 }
 
 
@@ -185,7 +187,6 @@ function prizeMaker() {         // should give me a random number of prizes star
     for(let i = 0; i < randInt; i++){       // adds it tpp the prizes arr
         prizeItem = new Prize;
         prizes.push(prizeItem);
-        //console.log(prizes[i]);
     }
 
     for(let i = 0; i < prizes.length; i++){
@@ -199,9 +200,11 @@ function prizeMaker() {         // should give me a random number of prizes star
 }
 
 // ==== Hit Detection ====
+let sweet_l, sweet_r;           //  this make it global but didnt wanna put it up there 
 
 function hitDetection(claw, prize) {
-
+    sweet_l = (claw.x + claw.aveWidth)/2;
+    sweet_r = (claw.x + 450)/2;
 }
     // ==== Time Left ====
 
